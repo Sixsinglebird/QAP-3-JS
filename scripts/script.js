1cf80b1aecfd5308ce8e293d1f25963949fc726b
Alex Singleton <alex.singleton@keyin.com>
2023-03-19 12:58:42 -0230

    updated CSS/HTML skeleton to reflect QAP3

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 // i want to make a website to practice handling data
-function loadAPI() {
-  let url = "https://api.github.com/users";
-  fetch(url)
+async function loadAPI() {
+  let url = "https://dog.ceo/api/breeds/list/al";
+  await fetch(url)
     .then((response) => response.json())
     .then((users) => {
       users.forEach((user) => {
         let output = `<div  class="profile"><img src= "${user.avatar_url}"><h3 id="login">${user.login}</h3><br><ul id="details"><li>id: ${user.id}</li><li>page: <a href="${user.html_url}">${user.html_url}</a></li></ul></div>`;
-        document.querySelector("#profiles").innerHTML += output;
+        document.querySelector("#dogs").innerHTML += output;
       });
     });
 }
 
-function idSearch(value) {
+async function idSearch(value) {
   let url = "https://api.github.com/users";
-  fetch(url)
+  await fetch(url)
     .then((response) => response.json())
     .then((users) => {
       users.forEach((user)=>{
