8b4b40bcdf9bd0eb8b3b3b6aff6ae8a082a97a74
Alex Singleton <alex.singleton@keyin.com>
2023-03-19 10:45:35 -0230

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+// i want to make a website to practice handling data
+function loadAPI() {
+  let url = "https://api.github.com/users";
+  fetch(url)
+    .then((response) => response.json())
+    .then((users) => {
+      users.forEach((user) => {
+        let output = `<div  class="profile"><img src= "${user.avatar_url}"><h3 id="login">${user.login}</h3><br><ul id="details"><li>id: ${user.id}</li><li>page: <a href="${user.html_url}">${user.html_url}</a></li></ul></div>`;
+        document.querySelector("#profiles").innerHTML += output;
+      });
+    });
+}
+
+function idSearch(value) {
+  let url = "https://api.github.com/users";
+  fetch(url)
+    .then((response) => response.json())
+    .then((users) => {
+      users.forEach((user)=>{
+        if (value == user.id) {
+          let output = `<div  class="profile"><img src= "${user.avatar_url}"><h3 id="login">${user.login}</h3><br><ul id="details"><li>id: ${user.id}</li><li>page: <a href="${user.html_url}">${user.html_url}</a></li></ul></div>`;
+          document.querySelector("#searched").innerHTML += output;
+        };  
+      });
+    });
+  };
+
+async function awaitComment() {
+  let comment = await fetch(`https://jsonplaceholder.typicode.com/comments`)
+}
+
+///////////////////////////////////////////////////
+window.onload = () => {
+  loadAPI();
+};
+
+// search button
+const searchBttn = document.querySelector("#search");
+searchBttn.addEventListener("click", () => {
+  idSearch(document.querySelector("#id").value);
+  document.querySelector("#id").value = "";
+});
