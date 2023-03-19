4e511dcfbccd2f70400992640abfb2a0dab3f930
Alex Singleton <alex.singleton@keyin.com>
2023-03-19 18:21:24 -0230

    started handling the data breed data received  from the API

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,13 @@
-// i want to make a website to practice handling data
-async function loadAPI() {
+// author: A Singleton
+/////////////////////////////////////////////////////
+// functions
+async function loadBreedsAPI() {
   let url = "https://dog.ceo/api/breeds/list/all";
   await fetch(url).then((response) => console.log(response.json()));
 }
-
-async function idSearch(value) {
-  let url = "https://api.github.com/users";
-  await fetch(url)
-    .then((response) => response.json())
-    .then((users) => {
-      users.forEach((user) => {
-        if (value == user.id) {
-          let output = `<div  class="profile"><img src= "${user.avatar_url}"><h3 id="login">${user.login}</h3><br><ul id="details"><li>id: ${user.id}</li><li>page: <a href="${user.html_url}">${user.html_url}</a></li></ul></div>`;
-          document.querySelector("#searched").innerHTML += output;
-        }
-      });
-    });
-}
-
-async function awaitComment() {
-  let comment = await fetch(`https://jsonplaceholder.typicode.com/comments`);
-}
-
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// load the doggo API
 window.onload = () => {
-  loadAPI();
+  loadBreedsAPI();
+  // loadImagesAPI();
 };
-
-// search button
-const searchBttn = document.querySelector("#search");
-searchBttn.addEventListener("click", () => {
-  idSearch(document.querySelector("#id").value);
-  document.querySelector("#id").value = "";
-});
