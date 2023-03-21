37255dc097741ddeb9a4333c7ab76698cf8d6628
Alex Singleton <alex.singleton@keyin.com>
2023-03-21 13:22:47 -0230

    uncommited

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,14 @@
 // functions
 async function loadBreedsAPI() {
   let url = "https://dog.ceo/api/breeds/list/all";
-  await fetch(url).then((response) => console.log(response.json()));
+  await fetch(url)
+    .then((response) => response.json())
+    .then((response2) => console.log(response2.message));
+  // .then((breeds) => {
+  //   breeds.forEach((breed) => console.log(breed));
+  // });
 }
+
 /////////////////////////////////////////////////////
 // load the doggo API
 window.onload = () => {
